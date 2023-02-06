b3d0fe5df95f0cb89ae17df98688daca55c07457
kuresato <kuresato@gmail.com>
2023-02-06 06:24:29 +0000

    add fail test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,8 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void test() {
+		fail("this is fail test");
+	}
 }
